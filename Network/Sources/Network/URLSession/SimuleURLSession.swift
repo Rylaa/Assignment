000f9faa0c468c6f9f042cd15cc688869868c4e3
Yusuf Demirkoparan <y.demirkoparan@papara.com>
2024-01-26 16:33:48 +0300

    Add SimuleURLSession to simulate URLSession requests

---
+++ Network/Sources/Network/URLSession/SimuleURLSession.swift
@@ -0,0 +1,33 @@
+//
+//  File.swift
+//
+//
+//  Created by Yusuf Demirkoparan on 26.01.2024.
+//
+
+import Foundation
+
+class SimuleURLSession: URLProtocol {
+    static var stubResponseData: Data?
+    static var stubURLResponse: HTTPURLResponse?
+    static var stubError: Error?
+
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+
+    override func startLoading() {
+        if let stubError = SimuleURLSession.stubError {
+            client?.urlProtocol(self, didFailWithError: stubError)
+        } else {
+            client?.urlProtocol(self, didLoad: SimuleURLSession.stubResponseData ?? Data())
+        }
+        client?.urlProtocolDidFinishLoading(self)
+    }
+
+    override func stopLoading() {}
+}
