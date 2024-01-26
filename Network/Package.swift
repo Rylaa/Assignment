b1f01253ecd7bf5efd8f075bdd5808e6a8811eef
Yusuf Demirkoparan <y.demirkoparan@papara.com>
2024-01-26 16:30:03 +0300

    Add platform and dependency for Network target

--- Network/Package.swift
+++ Network/Package.swift
@@ -5,17 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "Network",
+    platforms: [.iOS("14.0")],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Network",
             targets: ["Network"]),
     ],
+    dependencies: [
+        .package(path: "../Core")
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Network"),
+            name: "Network",
+            dependencies: ["Core"]),
         .testTarget(
             name: "NetworkTests",
             dependencies: ["Network"]),
