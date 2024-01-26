16030703d54a11abd4a91e9f0e42f7e907cccd6c
Yusuf Demirkoparan <y.demirkoparan@papara.com>
2024-01-26 16:30:57 +0300

    Add ApiConstants file with default values

---
+++ Network/Sources/Network/Layer/ApiConstants.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//
+//
+//  Created by Yusuf Demirkoparan on 26.01.2024.
+//
+
+import Foundation
+
+public struct ApiConstants {
+    public static let apiScheme: String = "https"
+    public static let apiHost: String = "TODO"
+    public static let baseUrl: String = "TODO"
+
+    public enum Path: String {
+        case cards
+    }
+}
