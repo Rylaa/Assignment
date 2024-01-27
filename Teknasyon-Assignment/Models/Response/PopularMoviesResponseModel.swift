c7d72b07fe0c133cf8e41a5a4a907a76b35eac1c
Yusuf Demirkoparan <y.demirkoparan@papara.com>
2024-01-27 14:26:58 +0300

    Refactor PopularMoviesInteractor's fetchPopularMovies method and provideCollectionViewSections method

--- Teknasyon-Assignment/Models/Response/PopularMoviesResponseModel.swift
+++ Teknasyon-Assignment/Models/Response/PopularMoviesResponseModel.swift
@@ -9,25 +9,84 @@ import Foundation
 
 struct PopularMoviesResponseModel: Codable, Hashable {
     let id: String? = UUID().uuidString
-    let page: Int?
     let results: [PopularMoviesDetailModel]?
     let totalPages: Int?
     let totalResults: Int?
+    let page: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+        case page
+    }
 }
 
 struct PopularMoviesDetailModel: Codable, Hashable {
+    let gender: Int?
+    let originalName: String?
+    let id: Int?
+    let adult: Bool?
+    let knownForDepartment: String?
+    let popularity: Double?
+    let profilePath: String?
+    let name: String?
+    let movies: [MovieModel]?
+
+    enum CodingKeys: String, CodingKey {
+        case gender
+        case originalName = "original_name"
+        case id
+        case adult
+        case knownForDepartment = "known_for_department"
+        case popularity
+        case profilePath = "profile_path"
+        case name
+        case movies = "known_for"
+    }
+}
+
+struct MovieModel: Codable, Hashable {
+    let id: Int?
     let adult: Bool?
     let backdropPath: String?
+    let originalTitle: String?
+    let mediaType: String?
     let genreIds: [Int]?
-    let id: Int?
-    let originCountry: [String]?
-    let originalLanguage: String?
-    let originalName: String?
-    let overview: String?
+    let voteAverage: Double?
     let popularity: Double?
     let posterPath: String?
-    let firstAirDate: String?
-    let name: String?
-    let voteAverage: Double?
+    let title: String?
+    let overview: String?
+    let originalLanguage: String?
     let voteCount: Int?
+    let releaseDate: String?
+    let video: Bool?
+    let originCountry: [String]?
+    let firstAirDate: String?
+    let originalName: String?
+    let name: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case adult
+        case backdropPath = "backdrop_path"
+        case originalTitle = "original_title"
+        case mediaType = "media_type"
+        case genreIds = "genre_ids"
+        case voteAverage = "vote_average"
+        case popularity
+        case posterPath = "poster_path"
+        case title
+        case overview
+        case originalLanguage = "original_language"
+        case voteCount = "vote_count"
+        case releaseDate = "release_date"
+        case video
+        case originCountry = "origin_country"
+        case firstAirDate = "first_air_date"
+        case originalName = "original_name"
+        case name
+    }
 }
--- Teknasyon-Assignment/Modules/PopularMovies/PopularMoviesInteractor.swift
+++ Teknasyon-Assignment/Modules/PopularMovies/PopularMoviesInteractor.swift
@@ -5,39 +5,64 @@
 //  Created by Yusuf Demirkoparan on 26.01.2024.
 //
 
-import Foundation
-import Core
 import Combine
+import Core
+import Foundation
 
 protocol PopularMoviesInteractorProtocol {
     var presenter: PopularMoviesPresenterProtocol? { get }
-  
+
     func fetchPopularMovies()
 }
 
 final class PopularMoviesInteractor {
     weak var presenter: PopularMoviesPresenterProtocol?
     var worker: PopularMoviesWorkerProtocol?
-    
+    private var response: PopularMoviesResponseModel?
+    private var paginationManager = PaginationManager()
     private var cancellables = CancelBag()
 }
 
 extension PopularMoviesInteractor: PopularMoviesInteractorProtocol {
-    
     @MainActor
     func fetchPopularMovies() {
-        worker?.fetchPopularMovies().sink(receiveCompletion: { [weak self] requestState in
-            guard let self else { return }
-            
-            switch requestState {
-            case .finished:
-                Log.debug("\(#function) request succeeded")
-            case .failure(let error):
-                Log.debug("Request failed with error: \(error)")
+        worker?.fetchPopularMoviesWithPage(paginationManager.currentPage)
+            .sink(receiveCompletion: { [weak self] requestState in
+                guard let self else { return }
+
+                switch requestState {
+                case .finished:
+                    Log.debug("\(#function) request succeeded")
+                case let .failure(error):
+                    Log.debug("\(#function) Request failed with error: \(error)")
+                }
+            }, receiveValue: { [weak self] response in
+                guard let self, let results = response.results else { return }
+                
+                self.response = response
+                self.updatePaginationManager()
+                self.provideCollectionViewSections(results)
+            })
+            .store(in: cancellables)
+    }
+    
+    func updatePaginationManager() {
+        paginationManager.nextPage()
+        
+        if paginationManager.totalPage == .zero {
+            paginationManager.totalPage = (response?.totalPages).toIntValueOrZero
+        }
+    }
+    
+    func provideCollectionViewSections(_ sections: [PopularMoviesDetailModel]) {
+        let sections = sections.compactMap { section -> Section<PopularMoviesCollectionHeaderModel?, [MovieModel]>? in
+            guard let movies = section.movies else {
+                return nil
             }
-        }, receiveValue: { [weak self] response in
-            guard let self else { return }
             
-        }).store(in: cancellables)
+            return Section(headerItem: PopularMoviesCollectionHeaderModel(title: section.name), items: movies)
+        }
+        
+        presenter?.addCollectionViewSections(sections)
     }
 }
