07e70af3744c1f41e4fd3affd2e22d8b397cab54
Yusuf Demirkoparan <y.demirkoparan@papara.com>
2024-01-27 21:44:57 +0300

    Add MoviesDetail module files

---
+++ Teknasyon-Assignment/Modules/MoviesDetail/MoviesDetailInteractor.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesDetailInteractor.swift
+//  Teknasyon-Assignment
+//
+//  Created by Yusuf Demirkoparan on 27.01.2024.
+//
+
+import Foundation
---
+++ Teknasyon-Assignment/Modules/MoviesDetail/MoviesDetailViewController.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesDetailViewController.swift
+//  Teknasyon-Assignment
+//
+//  Created by Yusuf Demirkoparan on 27.01.2024.
+//
+
+import Foundation
